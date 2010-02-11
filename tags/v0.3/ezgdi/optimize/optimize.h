b157aa0f2e30c22f9b14b599d92a90044e0c2a5d
quinn.liqin <quinn.liqin@3296684c-9210-11de-ba91-65e9557df13a>
2010-02-11 14:48:52 +0000

    32 bit DLL works perfectly like 0850. 64 bit DLL unbelievably works on my Win7 x64 yeah

---
+++ tags/v0.3/ezgdi/optimize/optimize.h
@@ -0,0 +1,13 @@
+#include "memcpy_amd.h"
+
+#ifdef _M_IX86
+#undef ZeroMemory
+#undef memzero
+#undef memset
+#undef memcpy
+
+#define ZeroMemory memzero_optimized
+#define memzero memzero_optimized
+#define memset memset_optimized
+#define memcpy memcpy_optimized
+#endif
